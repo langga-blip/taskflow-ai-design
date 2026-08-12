7d02e8ac246a2ad08c774060151e3d800a38b28b
langga-blip <mummom692@gmail.com>
2026-08-12 18:45:06 +0100

    Update dependencies and release configuration in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
                 signingConfig = signingConfigs.getByName("debugConfig")
             }
         }
+
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -65,6 +66,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
